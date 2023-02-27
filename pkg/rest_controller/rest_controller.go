a1ad1d87bd2ad0e3f60f644c9cfe327daad9ca7b
Sahan Thinusha <sahan@virtuan.io>
2023-02-27 23:16:53 +0530

    vehicle routes added

--- pkg/rest_controller/rest_controller.go
+++ pkg/rest_controller/rest_controller.go
@@ -2,6 +2,8 @@ package rest_controller
 
 import (
 	user "autotec/pkg/api/user"
+	vehicle "autotec/pkg/api/vehicle"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	echoswagger "github.com/swaggo/echo-swagger"
@@ -24,6 +26,10 @@ func NormalRoutes(g *echo.Group) {
 	g.POST("/v1/api/user", user.AddNewUser)
 	g.POST("/v1/api/user/login", user.Login)
 	g.GET("/v1/api/user", user.GetAllUsers)
+
+	g.POST("/v1/api/vehicle", vehicle.AddNewVehicle)
+	g.PUT("/v1/api/vehicle", vehicle.UpdateVehicle)
+	g.GET("/v1/api/vehicle", vehicle.GetVehiclesBtCustomer)
 }
 
 func SwaggerAPIDoc(g *echo.Group) {
