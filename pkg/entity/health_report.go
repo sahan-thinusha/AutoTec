f795df61f9b85bf8399ba691ee453817dc164a5e
Sahan Thinusha <sahan@virtuan.io>
2023-04-21 10:02:04 +0530

    changes

--- pkg/entity/health_report.go
+++ pkg/entity/health_report.go
@@ -6,12 +6,15 @@ type HealthReport struct {
 	Id               string `json:"id" bson:"_id"`
 	Base             `bson:",inline"`
 	Date             time.Time          `json:"date" bson:"date"`
-	VehicleId        string             `json:"VehicleId" bson:"VehicleId"`
+	VehicleId        string             `json:"vehicleId" bson:"vehicleId"`
+	VehicleName      string             `json:"vehicleName" bson:"vehicleName"`
 	CustomerId       string             `json:"customerId" bson:"customerId"`
+	CustomerName     string             `json:"customerName" bson:"customerName"`
 	HealthReportItem []HealthReportItem `json:"healthReportItem" bson:"healthReportItem"`
 }
 
 type HealthReportItem struct {
-	Part      string `json:"part" bson:"part"`
-	Condition string `json:"condition" bson:"condition"`
+	ID        string `json:"id" bson:"_id"`
+	Name      string `json:"name" bson:"name"`
+	Condition int    `json:"condition" bson:"condition"`
 }
