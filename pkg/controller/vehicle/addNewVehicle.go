deb74ad7e3c83e123eedc3e4064b252bf6cfc290
Sahan Thinusha <sahan@virtuan.io>
2023-04-24 10:44:44 +0530

    updates

--- pkg/controller/vehicle/addNewVehicle.go
+++ pkg/controller/vehicle/addNewVehicle.go
@@ -4,6 +4,7 @@ import (
 	"autotec/pkg/entity"
 	"autotec/pkg/env"
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
@@ -12,8 +13,15 @@ func AddNewVehicle(vehicle *entity.Vehicle) (*entity.Vehicle, error) {
 	currentTime := time.Now()
 	vehicle.CreatedAt = &currentTime
 	vehicle.UpdatedAt = &currentTime
+	user := entity.User{}
+
 	vehicle.Id = primitive.NewObjectID().Hex()
 	db := env.MongoDBConnection
+
+	data := db.Collection("Users").FindOne(context.Background(), bson.M{"_id": vehicle.UserId})
+	data.Decode(user)
+	vehicle.FirstName = user.FirstName
+	vehicle.LastName = user.LastName
 	_, err := db.Collection("Vehicle").InsertOne(context.Background(), vehicle)
 	if err != nil {
 		return nil, err
